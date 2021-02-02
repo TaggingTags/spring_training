5086c55ba0ce2f0e94ab534c8b9042d5c15cabb1
Tiago Santos <tstags.s@gmail.com>
2021-02-02 20:46:37 +0000

    first commit

---
+++ 2.web_api/web_api/src/test/java/tags/training/web_api/WebApiApplicationTests.java
@@ -0,0 +1,13 @@
+package tags.training.web_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
